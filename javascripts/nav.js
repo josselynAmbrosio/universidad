b6fb071d1747aec3bf9f32691d5042f47e5e9c17
josselynAmbrosio <josselynambrosioespiritu@gmail.com>
2015-05-20 07:43:58 +0100

    agregado nuevos cambios

---
+++ javascripts/nav.js
@@ -0,0 +1,11 @@
+$('.popover-markup > .trigger').popover({
+    html: true,
+    title: function () {
+        return $(this).parent().find('.head').html();
+    },
+    content: function () {
+        return $(this).parent().find('.content').html();
+    },
+    container: 'body',
+    placement: 'bottom'
+});
