b465af09ce5ec8ca9b1f85eb39605d8f1a9f6a1a
josselynAmbrosio <josselynambrosioespiritu@gmail.com>
2015-05-22 06:11:39 +0100

    agregado un efecto mas

---
+++ javascripts/nav.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+	$(".mostrar-ojo").click(function(){
+		$(this).parent().find(".faltas").toggle();
+		$(this).parent().find(".promedio").toggle();
+		$(this).hide();
+	});
+	$(".faltas").click(function(){
+		$(this).parent().find(".mostrar-ojo").toggle();
+		$(this).parent().find(".promedio").toggle();
+		$(this).hide();
+	});
+	$(".promedio").click(function(){
+		$(this).parent().find(".mostrar-ojo").toggle();
+		$(this).parent().find(".faltas").toggle();
+		$(this).hide();
+	});
+	$(".open-ojo").click(function(){
+		$(this).parent().find(".fecha").toggle();
+		$(this).parent().find(".mostrar").toggle();
+		$(this).hide();
+	});
+	$(".fecha").click(function(){
+		$(this).parent().find(".open-ojo").toggle();
+		$(this).parent().find(".mostrar").toggle();
+		$(this).hide();
+	});
+	$(".mostrar").click(function(){
+
+		$(this).parent().find(".open-ojo").toggle();
+		$(this).parent().find(".fecha").toggle();
+		$(this).hide();
+	});
+});
